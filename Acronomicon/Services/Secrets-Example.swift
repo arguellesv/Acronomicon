760d01b5b45300ff71123f06683228d2eb0a0e8e
Victor Arguelles <15826485+arguellesv@users.noreply.github.com>
2021-06-10 13:07:08 -0500

    Add Secrets-Example file

---
+++ Acronomicon/Services/Secrets-Example.swift
@@ -0,0 +1,20 @@
+//  Secrets-Example.swift
+//  Acronomicon
+//
+//  Created by Victor Argüelles on 10/06/21.
+//  
+//
+
+import Foundation
+
+struct SecretsExample {
+    // API Keys
+    
+    // Merriam-Webster API
+    static let merriamWebsterDictionaryAPIKey = "YOUR_DICTIONARY_API_KEY"
+    static let merriamWebsterLearnersAPIKey = "YOUR_LEARNER'S_API_KEY"
+    
+    // STANDS4 API
+    static let stands4UserID = "YOUR_USER_ID"
+    static let stands4Token = "YOUR_TOKEN"
+}
